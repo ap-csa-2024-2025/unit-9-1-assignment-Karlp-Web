f042e677d5f36d90e336ff1b86e15284a7029961
Karlp-Web <karlenportillo1@gmail.com>
2025-03-31 18:19:33 +0000

    yomomma

---
+++ SpecialtyCoffee.java
@@ -0,0 +1,14 @@
+public class SpecialtyCoffee extends Coffee
+{
+    private String flavor;
+
+    public SpecialtyCoffee(String size,
+                            boolean isSkinny,
+                            int shots,
+                            String type,
+                            String flavor)
+    {
+        super(size, isSkinny, shots, type);
+        this.flavor = flavor;
+    }
+}
